13d70d7c5fb127602fa8484617152bdf35254dcf
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-01-14 19:29:15 +0200

    Added the filter by category feature

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
   const [countriesAPI, setCountriesAPI] = useState([]);
   const [search, setSearch] = useState("");
   const [counter, setCounter] = useState(11);
-  console.log(search);
+  const [category, setCategory] = useState("none");
 
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all")
@@ -19,21 +19,22 @@ export const Home = () => {
         <div id="home-container">
           <div id="filter">
             <div id="search-container">
-              <input id="search" type="search" placeholder="Search for a country..." onChange={(e) => {document.querySelector("#load").style.display = "none"; setCounter(countriesAPI.length);setSearch(e.target.value)}} />
+              <input id="search" type="search" placeholder="Search for a country..." onChange={(e) => {document.querySelector("#load").style.display = "none"; setCounter(countriesAPI.length); setSearch(e.target.value)}} />
               <i className="fa-solid fa-magnifying-glass"></i>
             </div>
-            <select id="select" defaultValue="none">
+            <select id="select" defaultValue="none" onChange={(e) => {document.querySelector("#load").style.display = "none"; setCounter(countriesAPI.length); setCategory(e.target.value)}}>
               <option value="none" disabled hidden>Filter by Region</option>
-              <option value="Africa">Africa</option>
-              <option value="America">America</option>
-              <option value="Asia">Asia</option>
-              <option value="Europe">Europe</option>
-              <option value="Oceania">Oceania</option>
+              <option value="Africa" >Africa</option>
+              <option value="Americas" >Americas</option>
+              <option value="Asia" >Asia</option>
+              <option value="Europe" >Europe</option>
+              <option value="Oceania" >Oceania</option>
             </select>
           </div>
           <div id="list">
             <div id="list-container">
-              {countriesAPI.filter(country => search.toLowerCase() === "" ? country : country.name.common.toLowerCase().includes(search))
+              {countriesAPI.filter(country => category === "none" ? country : country.region === category)
+                           .filter(country => search.toLowerCase() === "" ? country : country.name.common.toLowerCase().includes(search))
                            .filter(country => countriesAPI.indexOf(country) <= counter)
                            .map(country => (<Country key={countriesAPI.indexOf(country)} image={country.flags.png} name={country.name.common} population={country.population} region={country.region} capital={country.capital} />))
               }
