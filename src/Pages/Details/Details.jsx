9d5fe7b427cc518c6f5a550c991bb355afb7a2ea
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-01-18 20:21:26 +0200

    Prepared the Deatils page's context setup

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { APIContext } from "../../App";
 import "../Styles/Details/Details.css";
 
 export const Details = () => {
+  const APIData = useContext(APIContext);
+  const index = Number(window.location.pathname.slice(1, window.location.pathname.length));
   return (
-    <div id="details">Details</div>
+    <div id="details">
+      <div id="details-container">
+        <button><i className="fa-solid fa-arrow-left"></i> Back</button>
+        <div id="details-content">
+        </div>
+      </div>
+    </div>
   )
 }
