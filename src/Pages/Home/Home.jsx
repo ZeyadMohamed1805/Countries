6291549eb954d8ac0811bd0fb3e3705e32ad7af6
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-01-21 17:44:40 +0200

    Fixed an issue with dark mode & rendering countries

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,7 +6,6 @@ import { APIContext } from '../../App';
 export const Home = () => {
   const APIData = useContext(APIContext);
   const [search, setSearch] = useState("");
-  const [counter, setCounter] = useState(11);
   const [category, setCategory] = useState("none");
 
   return (
@@ -14,10 +13,10 @@ export const Home = () => {
         <div id="home-container">
           <div id="filter">
             <div id="search-container">
-              <input id="search" type="text" placeholder="Search for a country..." onChange={(e) => {document.querySelector("#load").style.display = "none"; setCounter(APIData.length); setSearch(e.target.value)}} />
+              <input id="search" type="text" placeholder="Search for a country..." onChange={(e) => {setSearch(e.target.value)}} />
               <i className="fa-solid fa-magnifying-glass"></i>
             </div>
-            <select id="select" defaultValue="none" onChange={(e) => {document.querySelector("#load").style.display = "none"; setCounter(APIData.length); setCategory(e.target.value)}}>
+            <select id="select" defaultValue="none" onChange={(e) => {setCategory(e.target.value)}}>
               <option value="none" disabled hidden>Filter by Region</option>
               <option value="Africa" >Africa</option>
               <option value="Americas" >Americas</option>
@@ -30,12 +29,10 @@ export const Home = () => {
             <div id="list-container">
               {APIData.filter(country => category === "none" ? country : country.region === category)
                            .filter(country => search.toLowerCase() === "" ? country : country.name.common.toLowerCase().includes(search))
-                           .filter(country => APIData.indexOf(country) <= counter)
                            .map(country => (<Country key={APIData.indexOf(country)} id={APIData.indexOf(country)} image={country.flags.png} name={country.name.common} population={country.population} region={country.region} capital={country.capital} />))
               }
             </div>
           </div>
-          <button id="load" onClick={() => setCounter(previous => previous + 12)}>Load More</button>
         </div>
     </div>
   )
